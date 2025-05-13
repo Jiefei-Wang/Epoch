6918fea4c57cad1eb429928dc59a8995c6278cb4
Jiefei Wang <szwjf08@gmail.com>
2025-05-13 16:58:51 +0300

    export parent class functions

--- R/zzz.r
+++ R/zzz.r
@@ -3,4 +3,18 @@
 #' @import methods
 #' @importFrom ramify pprint
 #' @importFrom glue glue
+#' @export dim
+#' @export dimnames
+#' @export nrow
+#' @export ncol
+#' @export rownames
+#' @export colnames
+#' @export rowData
+#' @export colData
+#' @export metaData
+#' @export tblData
+#' @export tblData<-
+#' @export rowData<-
+#' @export colData<-
+#' @export metaData<-
 NULL
